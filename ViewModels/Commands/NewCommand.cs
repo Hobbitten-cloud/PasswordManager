39c2b53d8dc886fa011dbb41f2cdcaa1a8859283
Hobbitten <janenhansen@gmail.com>
2024-11-28 13:12:13 +0100

    Few edits and changes

---
+++ ViewModels/Commands/NewCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager.ViewModels.Commands
+{
+    public class NewCommand
+    {
+        //public event EventHandler CanExecuteChanged;
+
+        //public bool CanExecute(object parameter)
+        //{
+        //    return true;
+        //}
+
+        //public void Execute(object parameter)
+        //{
+        //    if (parameter is MainViewModel mvm)
+        //    {
+        //        mvm.AddDefaultManager();
+        //    }
+        //}
+    }
+}
